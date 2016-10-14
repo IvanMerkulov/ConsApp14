afd0ee7e2b7bb7a9523b234c47ffde4e854496ca
Ivan Merkulov <zZEXz@yandex.ru>
2016-10-14 20:40:58 +0300

    первое задание готов

--- ConsoleApplication14/Program.cs
+++ ConsoleApplication14/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication14
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Введите стороны трейугольника а затем б");
+            Rectangle rect = new Rectangle(3,2);
+            double tmp,tmp1;
+            tmp = rect.Area;
+           
+            tmp1 = rect.Perimetr;
+            
         }
     }
 }
---
+++ ConsoleApplication14/Rectangle.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Rectangle
+    {
+        
+        //поля
+        private double side1;
+        private double side2;
+
+        private double area;
+        private double perimetr;
+       
+        //методы
+        private double AreaCalculator()
+        {
+            return side1*Side2;
+        }
+        private double PerimeterCalculator()
+        {
+
+            return ((side1+side2)*2);
+        }
+        //свойства
+        public double Side1
+        {
+            set
+            {
+                side1 = value;
+            }
+            get
+            {
+                return side1;
+            }
+
+        }
+        public double Side2
+        {
+            set
+            {
+                side2 = value;
+            }
+            get
+            {
+                return side2;
+            }
+
+        }
+
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+        public double Perimetr
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+
+        }
+
+
+
+        //конструктор
+        public Rectangle()
+        {
+            Console.WriteLine("Конструктор по умолчанию");
+        }
+        //пользовательский конструктор
+        public Rectangle(double x1, double x2)
+        {
+            this.side1 = x1;
+            this.side2 = x2;
+        }
+    }
+}
